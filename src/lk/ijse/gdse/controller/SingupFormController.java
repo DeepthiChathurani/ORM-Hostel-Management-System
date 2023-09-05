d3c37623fcf67ae775fa052e7dd6753fac17a4d0
deepthichathurani64@gmail.com <deepthichathurani64@gmail.com>
2023-09-05 21:57:24 +0530

    singup Page added

---
+++ src/lk/ijse/gdse/controller/SingupFormController.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SingupFormController {
+    public AnchorPane anc;
+    public TextField txtUserName;
+    public TextField txtPassword;
+
+    public void btnSing(ActionEvent actionEvent) {
+    }
+}
