3064ef1453360c9b0198d67e834d0a5fd5fce145
deepthichathurani64@gmail.com <deepthichathurani64@gmail.com>
2023-09-08 22:33:52 +0530

    dto add

---
+++ src/lk/ijse/gdse/hostel_management_system/dto/ReservationDTO.java
@@ -0,0 +1,103 @@
+package lk.ijse.gdse.hostel_management_system.dto;
+
+import java.sql.Date;
+
+public class ReservationDTO {
+    private String resId;
+    private Date date;
+    private String status;
+
+    private StudentDTO studentDTO;
+    private RoomDTO roomDTO;
+    private String studentId;
+    private String roomId;
+
+
+
+    public ReservationDTO(String resId, Date date, String status) {
+        this.resId = resId;
+        this.date = date;
+        this.status = status;
+    }
+
+    public ReservationDTO(String resId, Date date, String status, StudentDTO studentDTO, RoomDTO roomDTO) {
+        this.resId = resId;
+        this.date = date;
+        this.status = status;
+        this.studentDTO = studentDTO;
+        this.roomDTO = roomDTO;
+        studentId=studentDTO.getStudentId();
+        roomId=roomDTO.getRoomTypeId();
+    }
+
+    public String getResId() {
+        return resId;
+    }
+
+    public void setResId(String resId) {
+        this.resId = resId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public StudentDTO getStudentDTO() {
+        return studentDTO;
+    }
+
+    public void setStudentDTO(StudentDTO studentDTO) {
+        this.studentDTO = studentDTO;
+        studentId=studentDTO.getStudentId();
+    }
+
+    public RoomDTO getRoomDTO() {
+        return roomDTO;
+    }
+
+    public void setRoomDTO(RoomDTO roomDTO) {
+        this.roomDTO = roomDTO;
+        roomId=roomDTO.getRoomTypeId();
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    @Override
+    public String toString() {
+        return "ReservationDTO{" +
+                "resId='" + resId + '\'' +
+                ", date=" + date +
+                ", status='" + status + '\'' +
+                ", studentDTO=" + studentDTO +
+                ", roomDTO=" + roomDTO +
+                ", studentId='" + studentId + '\'' +
+                ", roomId='" + roomId + '\'' +
+                '}';
+    }
+}
---
+++ src/lk/ijse/gdse/hostel_management_system/dto/StudentDTO.java
@@ -0,0 +1,82 @@
+package lk.ijse.gdse.hostel_management_system.dto;
+
+public class StudentDTO {
+    private String studentId;
+    private String name;
+    private String address;
+    private String contact;
+    private String dob;
+    private String gender;
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(String studentId, String name, String address, String contact, String dob, String gender) {
+        this.studentId = studentId;
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        this.dob = dob;
+        this.gender = gender;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentDTO{" +
+                "studentId='" + studentId + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", contact='" + contact + '\'' +
+                ", dob='" + dob + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
